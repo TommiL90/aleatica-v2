f63d51bde29ff5308ca2058e4103f3eee8dbaa70
Tomas Lillo Sanhueza <108248835+TommiL90@users.noreply.github.com>
2024-02-09 15:53:57 -0300

    refactoring app

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
-'use client'
-import Image from 'next/image'
-import { usePathname } from 'next/navigation'
+import Link from "next/link"
 
-export default function Home() {
-  const pathName = usePathname()
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
 
-  console.log(pathName)
+export default function IndexPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Beautifully designed components <br className="hidden sm:inline" />
+          built with Radix UI and Tailwind CSS.
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Accessible and customizable components that you can copy and paste
+          into your apps. Free. Open Source. And Next.js 13 Ready.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
   )
 }
